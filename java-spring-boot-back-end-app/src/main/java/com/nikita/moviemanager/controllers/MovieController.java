9ef0f7d7b1c41f4c43d6d51be020c1dbdb10d97b
Nikita Paul <nikitapaul229@gmail.com>
2026-09-15 21:33:51 -0500

    Add review delete endpoint

--- java-spring-boot-back-end-app/src/main/java/com/nikita/moviemanager/controllers/MovieController.java
+++ java-spring-boot-back-end-app/src/main/java/com/nikita/moviemanager/controllers/MovieController.java
@@ -3,12 +3,14 @@ package com.nikita.moviemanager.controllers;
 import com.nikita.moviemanager.models.Movie;
 import com.nikita.moviemanager.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("movies")
 public class MovieController {
 
