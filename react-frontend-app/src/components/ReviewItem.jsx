36f5611085c4649c9a14d39776eec3733d5bb40e
Nikita Paul <nikitapaul229@gmail.com>
2026-09-15 22:09:43 -0500

    Added review item component

---
+++ react-frontend-app/src/components/ReviewItem.jsx
@@ -0,0 +1,24 @@
+// Review item component
+function ReviewItem({ review, deleteReview }) {
+
+  // Display one review
+  return (
+    <div>
+
+      {/* Display the review rating */}
+      <p>Rating: {review.rating}</p>
+
+      {/* Display the review comment */}
+      <p>Comment: {review.comment}</p>
+
+      {/* Button for deleting the review */}
+      <button onClick={() => deleteReview(review.id)}>
+        Delete Review
+      </button>
+
+    </div>
+  )
+}
+
+// Export ReviewItem component
+export default ReviewItem
