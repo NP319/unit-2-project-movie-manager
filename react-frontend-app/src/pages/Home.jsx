3b5c1bed1e46a38850648607847b222f4962d836
Nikita Paul <nikitapaul229@gmail.com>
2026-09-21 13:49:45 -0500

    Added media queries for responsiveness

--- react-frontend-app/src/pages/Home.jsx
+++ react-frontend-app/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 // Import Link to navigate to the Movie List page
 import { Link } from 'react-router'
 
-
+// Import the vintage movie image
+import vintageFilm from '../assets/vintage-film.png'
 
 // Home page component
 function Home() {
@@ -10,17 +11,30 @@ function Home() {
 
       {/* Welcome section */}
       <section className="home-section">
-        
+
+        {/* Vintage movie background image */}
+        <img
+          className="home-image"
+          src={vintageFilm}
+          alt="Vintage movie film reel, film cans, and popcorn"
+        />
+
+      {/* Home page content */}
+      <div className="home-content">
+
         <h1>Movie Manager</h1>
 
         <p>
           Manage your favorite movies and share your reviews.
         </p>
 
-        {/* Link to the Movie List page */}
-        <Link className="view-movies-link" to="/movies">
-          View Movies
-        </Link>
+      {/* Link to the Movie List page */}
+      <Link className="view-movies-link" to="/movies">
+        View Movies
+      </Link>
+
+      </div>
+
       </section>
 
     </main>
